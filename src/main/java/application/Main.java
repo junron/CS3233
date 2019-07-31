71b87afd7a0cff67eda117a3e181aae8e201c58b
Lam Jun Rong <bobo999bobo@outlook.com>
2019-07-31 19:43:05 +0800

    Basic javafx

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    try {
+//      AnchorPane root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+      AnchorPane root = new AnchorPane();
+      Scene scene = new Scene(root, 400, 400);
+      primaryStage.setTitle("Title");
+//      scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
+      primaryStage.setScene(scene);
+      primaryStage.setResizable(false);
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+}
