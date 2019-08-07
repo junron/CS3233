6645883cc68c0e7ec7b297eaf7e1f679b21a0d59
Lam Jun Rong <bobo999bobo@outlook.com>
2019-08-07 18:35:11 +0800

    Simple interface

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -9,11 +10,10 @@ public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     try {
-//      AnchorPane root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-      AnchorPane root = new AnchorPane();
-      Scene scene = new Scene(root, 400, 400);
+      AnchorPane root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+      Scene scene = new Scene(root, 600, 400);
       primaryStage.setTitle("Title");
-//      scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
+      scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
       primaryStage.setScene(scene);
       primaryStage.setResizable(false);
       primaryStage.show();
