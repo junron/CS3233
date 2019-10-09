bc246ef95440b6ac9eadc2d845a11da8f00a43b2
Lam Jun Rong <bobo999bobo@outlook.com>
2019-10-09 17:30:40 +0800

    Lol HENGYUE SUCC

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,17 +3,23 @@ package application;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     try {
-//      AnchorPane root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+      //      AnchorPane root = FXMLLoader.load(getClass().getResource("/main.fxml"));
       AnchorPane root = new AnchorPane();
-      Scene scene = new Scene(root, 400, 400);
+      WebView webView = new WebView();
+      webView.getEngine().load(getClass().getResource("/gmaps.html").toString());
+      root.getChildren().add(webView);
+      webView.getEngine().documentProperty().addListener(observable -> {
+        System.out.println("load");
+      });
+      Scene scene = new Scene(root, 800, 400);
       primaryStage.setTitle("Title");
-//      scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
       primaryStage.setScene(scene);
       primaryStage.setResizable(false);
       primaryStage.show();
@@ -21,6 +27,7 @@ public class Main extends Application {
       e.printStackTrace();
     }
   }
+
   public static void main(String[] args) {
     launch(args);
   }
