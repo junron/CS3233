79cbcfdd3942a61ac654cea57e8d4afd2c555d32
Lam Jun Rong <bobo999bobo@outlook.com>
2019-10-09 20:09:37 +0800

    Hmm succ

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
@@ -19,6 +20,28 @@ public class Main extends Application {
         System.out.println("load");
         // Call this function
         webView.getEngine().executeScript("addMarker(1.3068198,103.7679341,'NUSH')");
+        webView.getEngine().executeScript("addMarker(1.3466843,103.8412392,'RI')");
+        webView.getEngine().executeScript("addMarker(1.3264739,103.8005659,'HCI')");
+        new Thread(()->{
+          try {
+            Thread.sleep(1500);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          Platform.runLater(()-> webView.getEngine().executeScript("showMarkers('NUSH')"));
+          try {
+            Thread.sleep(1500);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          Platform.runLater(()-> webView.getEngine().executeScript("showMarkers('RI')"));
+          try {
+            Thread.sleep(1500);
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          Platform.runLater(()-> webView.getEngine().executeScript("showMarkers('HCI')"));
+        }).start();
       });
       Scene scene = new Scene(root, 800, 400);
       primaryStage.setTitle("Title");
