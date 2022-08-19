5e7a39c168e2bf2f84d4e6dafb2fc500c4fe6daf
Lam Jun Rong <junron1@outlook.com>
2022-08-19 16:59:03 +0800

    Fix load path

--- src/main/java/serialize/FileOps.java
+++ src/main/java/serialize/FileOps.java
@@ -31,12 +31,11 @@ public class FileOps {
     }
 
     public static ArrayList<String> load(Stage stage) throws IOException {
-        // FileChooser fileChooser = new FileChooser();
-        // fileChooser.setTitle("Load Susco Simulation");
-        // fileChooser.setInitialDirectory(new File(System.getProperty("user.home") + "/Desktop"));
-        // fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Susco Simulation files", "*.susco"));
-        // File file = fileChooser.showOpenDialog(stage);
-        File file = new File(System.getProperty("user.home") + "/Desktop/random/susco/simplenet1.susco");
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Load Susco Simulation");
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home") + "/Desktop"));
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Susco Simulation files", "*.susco"));
+        File file = fileChooser.showOpenDialog(stage);
         if (file == null) return null;
         return load(file);
     }
